564a39ef567b0d65e711b906fcdc25fe909b9df0
infoklasse <93081182+infoklasse@users.noreply.github.com>
2021-10-24 18:46:54 +0200

    Add files via upload

---
+++ Coding: First Steps/01_Variablen/sketch.js
@@ -0,0 +1,19 @@
+// variabe speichern werte 
+function setup() {
+
+  createCanvas(500, 500);
+  background(0);
+  stroke(190);
+  strokeWeight(4);
+  
+  // definierte variable
+  var positionX = 10;
+  var positionY = 10;
+  var höhe = 200;
+  var breite = 200;
+
+
+  // rechteck mit werten der variablen
+  rect(positionX, positionY, breite, höhe);
+
+}
