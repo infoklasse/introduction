a60ef6ed45cdae9f2a7082b10d9c8117ff10f649
infoklasse <93081182+infoklasse@users.noreply.github.com>
2021-10-24 18:47:49 +0200

    Add files via upload

---
+++ Coding: First Steps/00_Template/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(220);
+  ellipse(50,50,80,random(50));
+  ellipse(50,50,80,50);
+
+
+
+  //LEARNINGS
+  //zeige draw spezifikationen 
+  // console log zeigen
+  // random werte zeigen
+  // random werte aufrunden (floor) zeigen
+
+}
